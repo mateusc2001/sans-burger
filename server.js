b6913be397d971a8cd55bd233ce3a8c8dd2b4d10
mateusc2001 <mateusc2001@gmail.com>
2022-01-18 11:43:31 -0300

    Ajustando apontamento

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = 'sans-burger';
+const nomeApp = 'sans-burger-front-end';
 const app = express();
 
-app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-    res.sendFile(`${__dirname}/dist/index.html`);
+    res.sendFile(`${__dirname}/dist/${nomeApp}/index.html`);
 });
 
 app.listen(process.env.PORT || 8080);
